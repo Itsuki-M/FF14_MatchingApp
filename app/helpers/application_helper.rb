5de04fbd3716b5ba49d317bc422f2c63f70514da
Itsuki-M <ituki130618aiko@gmail.com>
2023-11-09 18:27:37 +0900

    トップページの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title(page_title = '')
+    base_title = "FF14一緒に遊びませんか？"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+
+  end
 end
